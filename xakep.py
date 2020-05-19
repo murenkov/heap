82905baf93dd8fe3e8f6707d780a3684bd3e1991
Yaroslav Murenkov <17390550+murenkov@users.noreply.github.com>
2020-05-20 00:49:58 +0300

    Add files via upload

---
+++ xakep.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://xakep.ru")
+page = response.text
+soup = BeautifulSoup(page, 'html.parser')
+
+headings = map(lambda e: e.text, soup.select("h3.entry-title a span"))
+for h in headings:
+    print(h)
